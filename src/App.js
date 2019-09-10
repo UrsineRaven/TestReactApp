73757c52d9d9cace9df5fee1f6faef7b93956c27
Kaleb Commers <kaleb.commers@hexagonsi.com>
2019-09-10 13:14:06 -0500

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,37 +1,41 @@
 import React from 'react';
-import About from './About.js';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { ReactComponent as Logo } from './resources/logo.svg';
 
-function Index() {
-  return <h2>Home</h2>;
-}
+// Bootstrap components
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 
-function Users() {
-  return <h2>Users</h2>;
-}
+// Pages
+import About from './pages/About';
+import Home from './pages/Home';
+import Users from './pages/Users';
 
+// Router
 function AppRouter() {
   return (
     <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about/">About</Link>
-            </li>
-            <li>
-              <Link to="/users/">Users</Link>
-            </li>
-          </ul>
-        </nav>
+      <Container>
+        <Navbar collapseOnSelect expand="lg" bg="primary" variant="dark">
+          <Navbar.Brand href="/">
+            <Logo className="d-inline-block align-top logo" />
+            {' TestApp'}
+          </Navbar.Brand>
+          <Navbar.Toggle />
+          <Navbar.Collapse>
+            <Nav>
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/about/">About</Nav.Link>
+              <Nav.Link href="/users/">Users</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
 
-        <Route path="/" exact component={Index} />
+        <Route path="/" exact component={Home} />
         <Route path="/about/" component={About} />
         <Route path="/users/" component={Users} />
-      </div>
+      </Container>
     </Router>
   );
 }
