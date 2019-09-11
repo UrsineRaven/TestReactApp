5ded061b197aa5d7ca5e74463ab491eef6337331
Kaleb Commers <kaleb.commers@hexagonsi.com>
2019-09-11 15:28:09 -0500

    Added event type management

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,89 +8,27 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
 
-const testDataEventTypes = [
-  { name: '#1', id: '1' },
-  { name: 'test', id: '2' },
-  { name: 'meal', id: '3' }
-];
-
 const testDataEntries = [
-  { time: '12:34', event: '#1', id: '123' },
-  { time: '16:30', event: 'test', id: '235' },
-  { time: '17:24', event: 'meal', id: '332' }
+  { time: '12:34', event: '1', id: '123' },
+  { time: '16:30', event: '2', id: '235' },
+  { time: '17:24', event: '3', id: '332' },
+  { time: '2:19:06 PM', event: '4', id: '333' }
 ];
 
-function NewEventCard(props) {
-  const eventButtons = props.eventTypes.map(evt => {
-    return (
-      <Button
-        variant="secondary"
-        title={'New ' + evt.name}
-        key={evt.id}
-        onClick={() => props.evtClick(evt.id)}
-        style={{ width: '100%' }}
-        className="col-lg-2 col-md-3 col-sm-4 col-6"
-      >
-        {evt.name}
-      </Button>
-    );
-  });
-  return (
-    <Card border="primary">
-      <Card.Header>New Events:</Card.Header>
-      <Card.Body>
-        <Form>
-          <Form.Group as={Row} controlId="inputTime">
-            <Form.Label column xs="auto">
-              Time (if not now):
-            </Form.Label>
-            <Col>
-              <Form.Control type="time" name="Time" />
-            </Col>
-          </Form.Group>
-          <Row>{eventButtons}</Row>
-        </Form>
-      </Card.Body>
-    </Card>
-  );
-}
-
-function TableRow(props) {
-  return (
-    <tr>
-      <td className="small-col">{props.time}</td>
-      <td className="big-col">{props.event}</td>
-      <td className="btn-col">
-        <button className="text-danger symbol" onClick={props.onDelete}>
-          <span>&times;</span>
-        </button>
-      </td>
-    </tr>
-  );
-}
-
-function Home() {
+function Home(props) {
   const [rows, setRows] = useState(testDataEntries);
-  const [evtTypes] = useState(testDataEventTypes);
   const [updateTime, setUpdateTime] = useState(new Date().toLocaleString());
 
   useEffect(() => {
     setUpdateTime(new Date().toLocaleString());
-  }, [rows, evtTypes]);
+  }, [rows, props.evtTypes]);
 
   function handleEventClick(id) {
     const newId = Math.max(...rows.map(e => e.id)) + 1;
-    let eventName;
-    for (let i = 0; i < evtTypes.length; i++) {
-      if (evtTypes[i].id === id) {
-        eventName = evtTypes[i].name;
-        break;
-      }
-    }
 
     setRows(
       rows.concat([
-        { time: new Date().toLocaleTimeString(), event: eventName, id: newId }
+        { time: new Date().toLocaleTimeString(), event: id, id: newId }
       ])
     );
   }
@@ -105,11 +43,20 @@ function Home() {
     setRows(resultRows);
   }
 
+  const eventTypes = {};
+  props.evtTypes.forEach(type => {
+    eventTypes[type.id] = {
+      name: type.name,
+      formatting: type.formatting
+    };
+  });
+
   const tableRows = rows.map(row => {
     return (
       <TableRow
         time={row.time}
-        event={row.event}
+        event={eventTypes[row.event].name}
+        formatting={eventTypes[row.event].formatting}
         onDelete={() => handleDelete(row.id)}
         key={row.id}
       />
@@ -118,14 +65,11 @@ function Home() {
 
   return (
     <div>
-      <h5 class="mt-3">
+      <h5 className="mt-3">
         {'Page last updated: '}
         <small className="text-muted">{updateTime}</small>
       </h5>
-      <NewEventCard
-        eventTypes={testDataEventTypes}
-        evtClick={handleEventClick}
-      />
+      <NewEventCard eventTypes={props.evtTypes} evtClick={handleEventClick} />
       <Table striped bordered size="sm" className="mt-2">
         <thead>
           <tr>
@@ -137,6 +81,57 @@ function Home() {
         <tbody>{tableRows}</tbody>
       </Table>
     </div>
+  );
+}
+
+function TableRow(props) {
+  return (
+    <tr {...(props.formatting && JSON.parse(props.formatting))}>
+      <td className="small-col">{props.time}</td>
+      <td className="big-col">{props.event}</td>
+      <td className="btn-col">
+        <button className="text-danger symbol" onClick={props.onDelete}>
+          <span>&times;</span>
+        </button>
+      </td>
+    </tr>
+  );
+}
+
+function NewEventCard(props) {
+  const eventButtons = props.eventTypes.map(evt => {
+    return (
+      !evt.hidden && (
+        <Button
+          variant="secondary"
+          title={'New ' + evt.name}
+          key={evt.id}
+          onClick={() => props.evtClick(evt.id)}
+          style={{ width: '100%' }}
+          className="col-lg-2 col-md-3 col-sm-4 col-6"
+        >
+          {evt.name}
+        </Button>
+      )
+    );
+  });
+  return (
+    <Card border="primary">
+      <Card.Header>New Events:</Card.Header>
+      <Card.Body>
+        <Form>
+          <Form.Group as={Row} controlId="inputTime">
+            <Form.Label column xs="auto">
+              Time (if not now):
+            </Form.Label>
+            <Col>
+              <Form.Control type="time" />
+            </Col>
+          </Form.Group>
+          <Row>{eventButtons}</Row>
+        </Form>
+      </Card.Body>
+    </Card>
   );
 }
 
