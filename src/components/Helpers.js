182c4f7202911e16a76d9eed61b433db04adf730
Kaleb Commers <kaleb.commers@hexagonsi.com>
2019-09-12 17:10:14 -0500

    added dates to events & fixed new events w/ time

---
+++ src/components/Helpers.js
@@ -0,0 +1,31 @@
+/**
+ * Returns the ISO-formatted string for the local timezone of the specified Date object.
+ * @param {Date} date Date object for which to return the local ISO string.
+ * @returns {string} The ISO-formatted string corresponding to the local time of the specified Date.
+ */
+export const getLocalIsoString = function(date) {
+  return new Date(
+    date.getTime() - date.getTimezoneOffset() * 60000
+  ).toISOString();
+};
+
+/**
+ * Returns an array containing the local ISO-formatted date and time.
+ * @param {Date} date Date object for which to generate the local ISO strings.
+ * @returns {Array<string>} Array containing the local ISO-formatted date (index 0) and time (index 1).
+ */
+export const getLocalIsoDateAndTime = function(date) {
+  const isoString = getLocalIsoString(date);
+  return splitIsoString(isoString);
+};
+
+/**
+ * Returns an array containing the date and time.
+ * @param {string} isoString ISO string to split.
+ * @returns {Array<string>} Array containing the ISO-formatted date (index 0) and time (index 1).
+ */
+export const splitIsoString = function(isoString) {
+  const dateSplit = isoString.split('T');
+  const timeSplit = dateSplit[1].split('.');
+  return [dateSplit[0], timeSplit[0]];
+};
