12403a32c195ed8e76e2b6b5fe0d5b4116a58244
Kaleb Commers <kaleb.commers@hexagonsi.com>
2019-09-12 12:14:01 -0500

    refactored the app and added a history page

--- src/App.js
+++ src/App.js
@@ -1,98 +1,23 @@
 import React from 'react';
-import { useState } from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import { ReactComponent as Logo } from './resources/logo.svg';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 // Bootstrap components
 import Container from 'react-bootstrap/Container';
-import Navbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Nav';
 
-// Pages
+// Pages / Components
 import About from './pages/About';
-import Home from './pages/Home';
-import Types from './pages/Types';
+import DbRoutes from './components/DatabaseManagedRoutes';
+import NavigationBar from './components/NavigationBar';
 import Users from './pages/Users';
-
-// Test Data
-const testDataEventTypes = [
-  { name: '#1', id: '1' },
-  { name: 'test', id: '2' },
-  { name: 'meal', id: '3' },
-  {
-    name: 'Example',
-    id: '4',
-    formatting: '{"className":"table-warning", "style":{"fontStyle": "italic"}}'
-  },
-  { name: 'hidden-example', id: '5', hidden: true }
-];
 
 // Router
 function AppRouter() {
-  const [navCollapsed, setNavCollapsed] = useState(true);
-  const [evtTypes, setEvtTypes] = useState(testDataEventTypes);
-
-  function handleEditType(evt) {
-    const index = evtTypes.findIndex(e => e.id === evt.id);
-    if (index === -1) {
-      setEvtTypes(evtTypes.concat([evt]));
-    } else {
-      let cp = evtTypes.slice();
-      cp.splice(index, 1, evt);
-      setEvtTypes(cp);
-    }
-  }
-
-  function changeCollapsed(toggle) {
-    let collapse;
-    if (toggle) collapse = !navCollapsed;
-    else collapse = true;
-    setNavCollapsed(collapse);
-  }
-
   return (
     <Router>
       <Container>
-        <Navbar
-          expand="lg"
-          bg="primary"
-          variant="dark"
-          expanded={!navCollapsed}
-        >
-          <Navbar.Brand href="/">
-            <Logo className="d-inline-block align-top logo" />
-            {' TestApp'}
-          </Navbar.Brand>
-          <Navbar.Toggle onClick={() => changeCollapsed(true)} />
-          <Navbar.Collapse
-            className="justify-content-end"
-            onClick={() => changeCollapsed()}
-          >
-            <Nav>
-              <Nav.Link as={Link} to="/">
-                Home
-              </Nav.Link>
-              <Nav.Link as={Link} to="/type-management/">
-                Event Type Management
-              </Nav.Link>
-              <Nav.Link as={Link} to="/about/">
-                About
-              </Nav.Link>
-              <Nav.Link as={Link} to="/users/">
-                Users
-              </Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar>
+        <NavigationBar />
 
-        <Route path="/" exact render={() => <Home evtTypes={evtTypes} />} />
-        <Route
-          path="/type-management/"
-          exact
-          render={() => (
-            <Types evtTypes={evtTypes} onEditType={handleEditType} />
-          )}
-        />
+        {DbRoutes()}
         <Route path="/about/" component={About} />
         <Route path="/users/" component={Users} />
       </Container>
