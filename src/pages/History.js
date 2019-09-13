40480ddaf0b51bdb423b37e4f18facd77f605d2b
UrsineRaven <UrsineRaven@gmail.com>
2019-09-13 11:16:53 -0500

    1st pass at History page

--- src/pages/History.js
+++ src/pages/History.js
@@ -6,8 +6,16 @@ import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
+import EventTypeSelector from '../components/EventTypeSelector';
+import { getLocalIsoString } from '../components/Helpers';
 
 function History(props) {
+  const [type, setType] = useState('');
+  const [startDate, setStartDate] = useState('2000-01-01');
+  const [endDate, setEndDate] = useState(
+    getLocalIsoString(new Date()).split('T')[0]
+  );
+
   const eventTypes = {};
   props.evtTypes.forEach(type => {
     eventTypes[type.id] = {
@@ -18,7 +26,10 @@ function History(props) {
 
   const tableRows = props.rows
     .filter(row => {
-      return true; // need to apply filters here
+      const evtType = type ? row.event === type : true;
+      const evtStartDate = startDate ? row.date >= startDate : true;
+      const evtEndDate = endDate ? row.date <= endDate : true;
+      return evtType && evtStartDate && evtEndDate;
     })
     .sort((row1, row2) =>
       (row2.date + row2.time).localeCompare(row1.date + row1.time)
@@ -37,7 +48,15 @@ function History(props) {
 
   return (
     <>
-      <FilterCard eventTypes={props.evtTypes} />
+      <FilterCard
+        eventTypes={props.evtTypes}
+        typeValue={type}
+        typeChange={newType => setType(newType)}
+        startDate={startDate}
+        startDateChange={newDate => setStartDate(newDate)}
+        endDate={endDate}
+        endDateChange={newDate => setEndDate(newDate)}
+      />
       <Table striped bordered size="sm" className="mt-2">
         <thead>
           <tr>
@@ -63,23 +82,48 @@ function TableRow(props) {
 }
 
 function FilterCard(props) {
+  const [time, setTime] = useState('');
+
   return (
     <Card border="primary" className="mt-3">
       <Card.Header>Filter Events:</Card.Header>
       <Card.Body>
         <Form>
-          {/* Date (range?)  */}
-          {/* Event Type  */}
-          {/* Groups (by day, week?, month&year, year?)  */}
-          {/* Time range?  */}
-          <Form.Group as={Row} controlId="inputTime">
+          <EventTypeSelector
+            evtTypes={props.eventTypes}
+            value={props.typeValue}
+            onChange={newType => props.typeChange(newType.id)}
+            defaultLabel="Any Event Type"
+            inline
+          />
+          <Form.Group as={Row} controlId="inputEndDate">
             <Form.Label column xs="auto">
-              Time (if not now):
+              Newest Date
             </Form.Label>
             <Col>
-              <Form.Control type="time" />
+              <Form.Control
+                type="date"
+                value={props.endDate}
+                onChange={newDate => props.endDateChange(newDate.target.value)}
+              />
             </Col>
           </Form.Group>
+          <Form.Group as={Row} controlId="inputStartDate">
+            <Form.Label column xs="auto">
+              Oldest Date
+            </Form.Label>
+            <Col>
+              <Form.Control
+                type="date"
+                value={props.startDate}
+                onChange={newDate =>
+                  props.startDateChange(newDate.target.value)
+                }
+              />
+            </Col>
+          </Form.Group>
+          {/* TODO: Groups (by day, week?, month&year, year?) */}
+          {/* TODO: Time range? */}
         </Form>
       </Card.Body>
     </Card>
