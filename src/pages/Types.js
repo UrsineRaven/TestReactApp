54af9136dd43f5b6b910eb1632f58aaa321048aa
Kaleb Commers <kaleb.commers@hexagonsi.com>
2019-09-13 09:35:26 -0500

    clean up imports

--- src/pages/Types.js
+++ src/pages/Types.js
@@ -1,5 +1,4 @@
-import React from 'react';
-import { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Alert from 'react-bootstrap/Alert';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
