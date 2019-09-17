b0a78ccd1d4be53a3d0c4d854ca74b8bdbd90d91
UrsineRaven <UrsineRaven@gmail.com>
2019-09-17 15:46:04 -0500

    added offlineOnly setting first stages of local changes support

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -25,6 +25,7 @@ function Settings(props) {
                 onChange={newVal =>
                   props.onChangePollInterval(newVal.target.value)
                 }
+                disabled={props.offlineOnly}
               />
               <Form.Text as="small" className="text-muted">
                 The data on the main page will automatically refresh this often.
@@ -49,6 +50,9 @@ function Settings(props) {
             </Form.Group>
             <Form.Group controlId="setAllowOfflineLogging">
               <Form.Label>Allow Offline Logging</Form.Label>
+              <Form.Text as="small" className="text-danger">
+                Not fully implemented!
+              </Form.Text>
               <Form.Check
                 className="text-muted"
                 type="checkbox"
@@ -57,12 +61,40 @@ function Settings(props) {
                 onChange={evt =>
                   props.onChangeAllowOfflineLogging(evt.target.checked)
                 }
+                disabled={props.offlineOnly}
               />
               <Form.Text as="small" className="text-muted">
                 Allowing offline logging enables you to log events even when
                 your device cannot access the server. You're device will sync
                 with the server when it next has access. If the polling interval
                 is disabled, it will try to access the server every 5 minutes.
+              </Form.Text>
+            </Form.Group>
+            <hr />
+            <Form.Group controlId="setOfflineOnly">
+              <Form.Label>Offline Only</Form.Label>
+              <Form.Text as="small" className="text-danger">
+                Not fully implemented!
+              </Form.Text>
+              <Form.Check
+                className="text-muted"
+                type="checkbox"
+                label="Never try to interact with server"
+                checked={props.offlineOnly}
+                onChange={evt => props.onChangeOfflineOnly(evt.target.checked)}
+              />
+              <Form.Text as="small" className="text-muted">
+                Enabling offline only prevents the app from trying to pull
+                changes from the server or push changes to the server. It stores
+                all changes locally in the browser cache. When this mode is
+                disabled, it will try to sync with the server again, pushing all
+                changes that have been made since the mode was enabled.
+              </Form.Text>
+              <Form.Text as="small" className="text-muted">
+                <strong>
+                  Note: It will still pull changes to the site itself, just not
+                  the data.
+                </strong>
               </Form.Text>
             </Form.Group>
           </Form>
