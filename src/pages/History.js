fd953ecfcc36bf0d2eed0f00450666355721fa8d
Kaleb Commers <kaleb.commers@hexagonsi.com>
2019-09-20 16:14:56 -0500

    Add date grouping options to Event History page

--- src/pages/History.js
+++ src/pages/History.js
@@ -8,7 +8,9 @@ import EventTypeSelector from '../components/EventTypeSelector';
 import {
   getLocalIsoDateAndTime,
   getLocalIsoString,
-  getStartAndEndDatetimes
+  getStartAndEndDatetimes,
+  getWeekNumber,
+  getWeekStartAndEnd
 } from '../components/Helpers';
 import PageHeading from '../components/PageHeading';
 
@@ -18,6 +20,7 @@ function History(props) {
   const [endDate, setEndDate] = useState(
     getLocalIsoString(new Date()).split('T')[0]
   );
+  const [groupBy, setGroupBy] = useState('');
 
   const eventTypes = {};
   props.evtTypes.forEach(type => {
@@ -27,21 +30,100 @@ function History(props) {
     };
   });
 
+  // filter rows and sort descending by date and time
   const [startDatetime, endDatetime] = getStartAndEndDatetimes(
     startDate,
     endDate
   );
-  const tableRows = props.rows
+  let processedRows = props.rows
     .filter(row => {
       const evtType = type ? row.event === type : true;
       const evtStartDate = startDatetime ? row.datetime >= startDatetime : true;
       const evtEndDate = endDatetime ? row.datetime <= endDatetime : true;
       return evtType && evtStartDate && evtEndDate;
     })
-    .sort((row1, row2) => row2.datetime - row1.datetime) // sort descending by date and time
-    .map(row => {
+    .sort((row1, row2) => row2.datetime - row1.datetime);
+
+  // add grouping rows
+  let currentGroup = '';
+  const monthNames = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+  ]; // TODO: use internationalization API once it's more supported (https://stackoverflow.com/a/18648314) (https://caniuse.com/#search=intl)
+  processedRows.forEach((row, index) => {
+    if (groupBy) {
+      let date, day;
+      switch (groupBy) {
+        case 'day':
+          date = new Date(row.datetime);
+          [day] = getLocalIsoDateAndTime(date);
+          if (currentGroup !== day) {
+            currentGroup = day;
+            processedRows.splice(index, 0, {
+              group: date.toDateString()
+            });
+          }
+          break;
+        case 'week':
+          date = new Date(row.datetime);
+          [day] = getLocalIsoDateAndTime(date);
+          const [wYear, wNo] = getWeekNumber(day);
+          const weekYear = wYear + '-' + wNo;
+          if (currentGroup !== weekYear) {
+            currentGroup = weekYear;
+            const [begin, end] = getWeekStartAndEnd(day);
+            processedRows.splice(index, 0, {
+              group:
+                'Week of: ' + begin.toDateString() + ' - ' + end.toDateString()
+            });
+          }
+          break;
+        case 'month':
+          date = new Date(row.datetime);
+          [day] = getLocalIsoDateAndTime(date);
+          const monthYear = day.slice(0, 7);
+          if (currentGroup !== monthYear) {
+            currentGroup = monthYear;
+            processedRows.splice(index, 0, {
+              group: monthNames[date.getMonth()] + ' ' + day.split('-')[0]
+            });
+          }
+          break;
+        case 'year':
+          date = new Date(row.datetime);
+          [day] = getLocalIsoDateAndTime(date);
+          const year = day.split('-')[0];
+          if (currentGroup !== year) {
+            currentGroup = year;
+            processedRows.splice(index, 0, {
+              group: year
+            });
+          }
+          break;
+        default:
+          break;
+      }
+    }
+  });
+
+  // convert rows to JSX
+  const tableRows = processedRows.map(row => {
+    let tableRow;
+    if (row.group) {
+      tableRow = <GroupRow groupName={row.group} />;
+    } else {
       const [date, time] = getLocalIsoDateAndTime(new Date(row.datetime));
-      return (
+      tableRow = (
         <TableRow
           date={date}
           time={time}
@@ -50,7 +132,9 @@ function History(props) {
           key={row.id}
         />
       );
-    });
+    }
+    return tableRow;
+  });
 
   return (
     <>
@@ -63,6 +147,8 @@ function History(props) {
         startDateChange={newDate => setStartDate(newDate)}
         endDate={endDate}
         endDateChange={newDate => setEndDate(newDate)}
+        groupBy={groupBy}
+        groupByChange={newGroup => setGroupBy(newGroup)}
       />
       <Table striped bordered size="sm" className="mt-2">
         <thead>
@@ -75,6 +161,14 @@ function History(props) {
         <tbody>{tableRows}</tbody>
       </Table>
     </>
+  );
+}
+
+function GroupRow(props) {
+  return (
+    <tr className="table-dark" style={{ fontWeight: 'bold' }}>
+      <td colSpan="3">{props.groupName}</td>
+    </tr>
   );
 }
 
@@ -127,7 +221,25 @@ function FilterCard(props) {
               />
             </Col>
           </Form.Group>
-          {/* TODO: Groups (by day, week?, month&year, year?) */}
+          <Form.Group as={Row} controlId="chooseGrouping">
+            <Form.Label column xs="auto">
+              Group By
+            </Form.Label>
+            <Form.Control
+              as="select"
+              value={props.groupBy}
+              // TODO: be more consistent with my anonymous handler function naming and structure
+              onChange={evt => props.groupByChange(evt.target.value)}
+              className="col mx-3"
+              style={{ minWidth: '9rem' }}
+            >
+              <option value="">{'--- None ---'}</option>
+              <option value="day">{'Day'}</option>
+              <option value="week">{'Week'}</option>
+              <option value="month">{'Month & Year'}</option>
+              <option value="year">{'Year'}</option>
+            </Form.Control>
+          </Form.Group>
           {/* TODO: Time range? */}
         </Form>
       </Card.Body>
